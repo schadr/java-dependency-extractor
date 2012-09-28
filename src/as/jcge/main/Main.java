89be9dc46dc1ed055ecc60929098bdbd104116fd
Adrian Schroeter <adrian.schroeter@googlemail.com>
2012-09-28 16:22:33 -0700

    changed main file to iterate over all callgraphs

--- src/as/jcge/main/Main.java
+++ src/as/jcge/main/Main.java
@@ -2,60 +2,21 @@ package as.jcge.main;
 
 import java.util.Map;
 
-import as.jcge.db.DatabaseConnector;
+import as.jcge.models.CallGraph;
+import as.jcge.output.XMLOutputter;
+import as.jcge.scm.SCMIterator;
+import as.jcge.scm.git.GitController;
 
 
 public class Main {
-	
-	
 	public static void main(String[] args) {
 		Map<String,String> optArgs = ArgumentParser.parseArguments(args);
-		
-		
-		System.out.println("TNG developed by eggnet.");
-		System.out.println();
-		try {
-			if (args.length < 4 ) {
-				System.out.println("Retry: TNG [dbname] [repository] [branch] [configFile] <commitFile> <port>");
-				throw new ArrayIndexOutOfBoundsException();
-			} else {
-				try  {
-					// Set up the resources
-					Resources.dbName = args[0];
-					Resources.repository = args[1];
-					Resources.branch = args[2];
-					Resources.configFile = args[3];
-					setRepositoryName(args[1]);
-					
-					// Set up custom port number
-					if(args.length >= 6) {
-						Resources.dbPort = args[5];
-					}
-					Resources.dbUrl = Resources.dbUrl + Resources.dbPort + "/";
-					System.out.println(Resources.dbUrl);
-					
-					DatabaseConnector db = new DatabaseConnector();
-					db.connect("eggnet");
-					db.createDatabase(Resources.dbName);
-					
-					NetworkBuilder nb = new NetworkBuilder(db);
-					
-					if(args.length >= 5)
-						nb.buildNetworks(args[4]);
-					else
-						nb.buildNetworks();
-					
-					db.close();
-				} catch (Exception e) {
-					e.printStackTrace();
-				} 
-			}
-		} catch (ArrayIndexOutOfBoundsException e) {
-			e.printStackTrace();
+		GitController gc = new GitController(optArgs.get(ArgumentParser.OPT_REPOSITORY_LOCATION));
+		SCMIterator iter = new SCMIterator(gc);
+		XMLOutputter outputter = new XMLOutputter();
+		while (iter.hasNext()) {
+			CallGraph cg = iter.next();
+			outputter.output(cg);
 		}
-	}
-	
-	private static void setRepositoryName(String path) {
-		Resources.repositoryName = path.substring(path.lastIndexOf("/")+1);
 	}
 }
